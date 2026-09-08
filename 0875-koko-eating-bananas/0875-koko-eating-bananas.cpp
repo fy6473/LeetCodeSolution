0e3c0158edf22bd5c21683ff46883146474866e9
priyanshu shukla <ankurshukla1223@gmail.com>
2026-09-08 22:53:17 +0530

    Time: 7 ms (69.55%), Space: 23.1 MB (5.52%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-        int themaximum(vector<int>& piles){
-            int maxi=0;
-            for(int i=0;i<piles.size();i++){
-                maxi=max(piles[i],maxi);
-            }
-            return maxi;
-        }
-        int calculatetotaltime(vector<int>& piles,int hourly){
-                int totaltime=0;
-            for(int i=0;i<piles.size();i++){
-                totaltime+=ceil((double)piles[i]/(double)hourly);
-            }
-            return totaltime;
-        }
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int high=themaximum(piles);
-            int totalh=0;
-            // int ans=0;
-            int low=1;
+int findmaximum(vector<int>& piles){
+    int maxi=0;
+    for(int i=0;i<piles.size();i++){
+        maxi=max(piles[i],maxi);
+    }
+    return maxi;
+}
+
+int calculatehour(vector<int>& piles, int hours){
+    int totalsum=0;
+    for(int i=0;i<piles.size();i++){
+        totalsum+=ceil((double)piles[i]/(double)hours);
+    }
+    return totalsum;
+}
+
+int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high=findmaximum(piles);
+        int totalh=0;
         for(;low<high;){
             int mid=(low+high)/2;
-            totalh=calculatetotaltime(piles,mid);
-            if(totalh<=h){
-                // ans=totalh;
-                high=mid;
-            }
-            else{
-                low=mid+1;
-            }
+            totalh=calculatehour(piles,mid);
+    if(totalh<=h){
+        high=mid;
+    }
+    else{
+        low=mid+1;
+    }
         }
         return low;
     }
