ae2f6dc90581e24fa6c5b2922866f47ef273b204
priyanshu shukla <ankurshukla1223@gmail.com>
2026-09-19 17:37:05 +0530

    Time: 88 ms (25.38%), Space: 88.9 MB (67.53%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -23,31 +23,61 @@ public:
         // }
         // return maxi;
 
-        if(nums.empty()){
-            return 0;
-        }
 
-    sort(nums.begin(),nums.end());
-        int n=nums.size();
-    int count=0;
-    int lastSmall=INT_MIN;
-    int maxi=1;
-    for(int i=0;i<n;i++){
-        if(nums[i]==lastSmall){
-            continue;
-        }
+// better soln:-
+    //     if(nums.empty()){
+    //         return 0;
+    //     }
+
+    // sort(nums.begin(),nums.end());
+    //     int n=nums.size();
+    // int count=0;
+    // int lastSmall=INT_MIN;
+    // int maxi=1;
+    // for(int i=0;i<n;i++){
+    //     if(nums[i]==lastSmall){
+    //         continue;
+    //     }
         
-        else if(nums[i]-1!=lastSmall){
-            lastSmall=nums[i];
-            count=1;
-             }
-             else{
-                count++;
-                lastSmall++;
-             }
+    //     else if(nums[i]-1!=lastSmall){
+    //         lastSmall=nums[i];
+    //         count=1;
+    //          }
+    //          else{
+    //             count++;
+    //             lastSmall++;
+    //          }
 
-         maxi=max(count,maxi);
-         }
-         return maxi;
+    //      maxi=max(count,maxi);
+    //      }
+    //      return maxi;
+     
+
+    //  optimal soln:-
+    int n=nums.size();
+    if(n==0){
+        return 0;
+    }
+    unordered_set<int> st;
+    for(int i=0;i<n;i++){
+        st.insert(nums[i]);
+    }
+    int maxi=1;
+    for(auto it:st){
+        if(st.find(it-1)==st.end()){
+            int cnt=1;
+            int x=it;
+            while(st.find(x+1)!=st.end()){
+                cnt++;
+                x=x+1;
+            }
+        maxi=max(cnt,maxi);
+        }
+    }
+    return maxi;
+
+
+
+
     }
 };
