907bf6f1374f802034fb77dfa6bae428abbc576b
priyanshu shukla <ankurshukla1223@gmail.com>
2026-09-21 21:18:03 +0530

    Time: 0 ms (100%), Space: 15.4 MB (88.66%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,36 +11,54 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head==NULL || head->next==NULL){
-            return head;
-        }
-         ListNode* temp=head;
-         vector<int> arr;
-         while(temp!=nullptr &&  temp->next!=nullptr){
-            arr.push_back(temp->val);
-            temp=temp->next->next;
-         }
-         if(temp){
-            arr.push_back(temp->val);
-         }
-          temp=head->next;
-          while(temp!=nullptr && temp->next!=nullptr){
-             arr.push_back(temp->val);
-            temp=temp->next->next;
-          }
-          if(temp){
-            arr.push_back(temp->val);
-         }
-        temp=head;
-        int i=0;
-        while(temp!=nullptr){
-            temp->val=arr[i];
-            temp=temp->next;
-            i++;
-        }
+        // brute force O(2n).
+//         if(head==NULL || head->next==NULL){
+//             return head;
+//         }
+//          ListNode* temp=head;
+//          vector<int> arr;
+//          while(temp!=nullptr &&  temp->next!=nullptr){
+//             arr.push_back(temp->val);
+//             temp=temp->next->next;
+//          }
+//          if(temp){
+//             arr.push_back(temp->val);
+//          }
+//           temp=head->next;
+//           while(temp!=nullptr && temp->next!=nullptr){
+//              arr.push_back(temp->val);
+//             temp=temp->next->next;
+//           }
+//           if(temp){
+//             arr.push_back(temp->val);
+//          }
+//         temp=head;
+//         int i=0;
+//         while(temp!=nullptr){
+//             temp->val=arr[i];
+//             temp=temp->next;
+//             i++;
+//         }
          
 
-return head;
-        
+// return head;
+
+
+
+// optimal:-O(n).
+if(head==nullptr || head->next==NULL){
+    return head;
+}
+ ListNode* odd=head;
+ ListNode* even=head->next;
+  ListNode* connect=head->next;
+  while(even!=nullptr && even->next!=nullptr){
+    odd->next=odd->next->next;
+    odd=odd->next;
+    even->next=even->next->next;
+    even=even->next;
+  }
+  odd->next=connect;
+  return head;
     }
 };
