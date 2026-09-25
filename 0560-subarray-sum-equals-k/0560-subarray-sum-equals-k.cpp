ad1bd58f6216f1ed2cdc54503d3e13e38829b7bc
priyanshu shukla <ankurshukla1223@gmail.com>
2026-09-25 21:34:12 +0530

    Time: 48 ms (44.08%), Space: 51.1 MB (13.24%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // brute force soln:-
+        // brute force soln:-O(n3)
         // int n=nums.size();
         // int cnt=0;
         // for(int i =0;i<n;i++){
@@ -17,18 +17,33 @@ public:
         // }
         // return cnt;
 
-        // better soln;
-        int n=nums.size();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum=sum+nums[j];
-               if(sum==k){
-                cnt++;
-                }
-            }
-        }
-    return cnt;
+        // better soln; o(n2).
+    //     int n=nums.size();
+    //     int cnt=0;
+    //     for(int i=0;i<n;i++){
+    //         int sum=0;
+    //         for(int j=i;j<n;j++){
+    //             sum=sum+nums[j];
+    //            if(sum==k){
+    //             cnt++;
+    //             }
+    //         }
+    //     }
+    // return cnt;
+    // }
+
+    // optimal soln:-O(n*logN).
+    int n=nums.size();
+    unordered_map<int,int> mpp;
+  mpp[0]=1;
+    int cnt=0;
+    int prefixsum=0;
+ for(int i=0;i<n;i++){
+     prefixsum=prefixsum+nums[i];
+     int remove=prefixsum-k;
+     cnt=cnt+mpp[remove];
+     mpp[prefixsum]=mpp[prefixsum]+1;    //mp[prefixsum]+=1;
+ }
+return cnt;
     }
 };
