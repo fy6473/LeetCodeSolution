524c1da4ecef19ca84d8804ef2ec865358237105
priyanshu shukla <ankurshukla1223@gmail.com>
2026-09-26 12:04:28 +0530

    Time: 7 ms (40.02%), Space: 26.6 MB (23.09%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         // brute force:- O(n2) and O(1) space.(rxcluding the output vector).
-        int n = nums.size();
-        vector<int> st;
+        // int n = nums.size();
+        // vector<int> st;
 
-        for(int i = 0; i < n; i++) {
-            if(find(st.begin(), st.end(), nums[i]) != st.end()) {
-                continue;
-            }
-            int cnt = 1;
-            int element = nums[i];
+        // for(int i = 0; i < n; i++) {
+        //     if(find(st.begin(), st.end(), nums[i]) != st.end()) {
+        //         continue;
+        //     }
+        //     int cnt = 1;
+        //     int element = nums[i];
 
-            for(int j = i + 1; j < n; j++) {
-                if(element == nums[j]) {
-                    cnt++;
-                }
-            }
+        //     for(int j = i + 1; j < n; j++) {
+        //         if(element == nums[j]) {
+        //             cnt++;
+        //         }
+        //     }
 
-            if(cnt >floor(n/3)) {
-                st.push_back(nums[i]);
-            }
-        }
+        //     if(cnt >floor(n/3)) {
+        //         st.push_back(nums[i]);
+        //     }
+        // }
 
-        return st;
+        // return st;
 
 
-        // better soln:-0(n) average . space:-O(n).
+        // better soln:-0(n) average and can be O(n*logN) . space:-O(n).
     //    int n=nums.size();
     //    unordered_map<int,int> mpp;
     //    vector<int> arr;
@@ -40,6 +40,20 @@ public:
     //    }
     //    return arr;
 
+
+// if you want more better soln:-
+    int n=nums.size();
+       unordered_map<int,int> mpp;
+       vector<int> arr;
+       int minimum=floor(n/3)+1;
+       for(int i=0;i<n;i++){
+        mpp[nums[i]]++;
+        if(mpp[nums[i]]==minimum){
+            arr.push_back(nums[i]);
+        }
+        if(arr.size()==2) break;
+       }
+       return arr;
 
 
     // optimal soln:-
