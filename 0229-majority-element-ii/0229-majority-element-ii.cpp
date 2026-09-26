f98e00cca7957e34f9affeffb203864b3e4f2252
priyanshu shukla <ankurshukla1223@gmail.com>
2026-09-26 11:41:01 +0530

    Time: 462 ms (5.07%), Space: 23.8 MB (77.19%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,44 +1,48 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        // brute forec:- O(n2) and O(1) space.(rxcluding the output vector).
-        // int n = nums.size();
-        // vector<int> st;
+        // brute force:- O(n2) and O(1) space.(rxcluding the output vector).
+        int n = nums.size();
+        vector<int> st;
 
-        // for(int i = 0; i < n; i++) {
-        //     int cnt = 1;
-        //     int element = nums[i];
+        for(int i = 0; i < n; i++) {
+            if(find(st.begin(), st.end(), nums[i]) != st.end()) {
+                continue;
+            }
+            int cnt = 1;
+            int element = nums[i];
 
-        //     for(int j = i + 1; j < n; j++) {
-        //         if(element == nums[j]) {
-        //             cnt++;
-        //         }
-        //     }
+            for(int j = i + 1; j < n; j++) {
+                if(element == nums[j]) {
+                    cnt++;
+                }
+            }
 
-        //     if(cnt >floor(n/3)) {
-        //         if(find(st.begin(), st.end(), nums[i]) != st.end()) {
-        //             continue;
-        //         }
-
-        //         st.push_back(nums[i]);
-        //     }
-        // }
-
-        // return st;
-
-
-        // better soln:-
-       int n=nums.size();
-       unordered_map<int,int> mpp;
-       vector<int> arr;
-       for(int i=0;i<n;i++){
-        mpp[nums[i]]++;
-       }
-       for(auto it:mpp){
-        if(it.second > floor(n/3)){
-            arr.push_back(it.first);
+            if(cnt >floor(n/3)) {
+                st.push_back(nums[i]);
+            }
         }
-       }
-       return arr;
+
+        return st;
+
+
+        // better soln:-0(n) average . space:-O(n).
+    //    int n=nums.size();
+    //    unordered_map<int,int> mpp;
+    //    vector<int> arr;
+    //    for(int i=0;i<n;i++){
+    //     mpp[nums[i]]++;
+    //    }
+    //    for(auto it:mpp){
+    //     if(it.second > floor(n/3)){
+    //         arr.push_back(it.first);
+    //     }
+    //    }
+    //    return arr;
+
+
+
+    // optimal soln:-
+
     }
 };
