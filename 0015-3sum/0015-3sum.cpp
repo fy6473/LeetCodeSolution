2d15e4ab4e3ee744201ba70c74a7099fcd89a129
priyanshu shukla <ankurshukla1223@gmail.com>
2026-09-27 00:00:35 +0530

    Time: 59 ms (18.98%), Space: 31 MB (19.91%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // t.c:-O(n3) and space is o(no.of unique triplets)*2.
+        // int n=nums.size();
+        //     set<vector<int>> st;
+        //     for(int i=0;i<n;i++){
+        //         for(int j=i+1;j<n;j++){
+        //             for(int k=j+1;k<n;k++){
+        //                 if(nums[i]+nums[j]+nums[k]==0){
+        //                     vector<int> temp={nums[i],nums[j],nums[k]};
+        //                     sort(temp.begin(),temp.end());
+        //                     st.insert(temp);
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     vector<vector<int>> arr(st.begin(),st.end());
+        //     return arr;
+
+
+        // better soln:-  using hashing:-
+        // t.c.:-o(n2*logM) && space is o(N)+o(no.of unique triplets)*2
+        // int n=nums.size();
+        // set<vector<int>> st;
+        // for(int i=0;i<n;i++){
+        //     set<int> hash;
+        //     for(int j=i+1;j<n;j++){
+        //         int k=-(nums[i]+nums[j]);
+        //         if(hash.find(k)!=hash.end()){
+        //             vector<int> temp={nums[i],nums[j],k};
+        //             sort(temp.begin(),temp.end());
+        //             st.insert(temp);
+        //         }
+        //         hash.insert(nums[j]);
+        //     }
+        // }
+        // vector<vector<int>> arr(st.begin(),st.end());
+        // return arr;
+
+
+        // optimal soln:-
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> arr;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                 if(sum<0){
+                    j++;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    vector<int>temp={nums[i],nums[j],nums[k]};
+                    arr.push_back(temp);
+                    j++;
+                    k--;
+                    // edge case:-
+                    while(j<k && nums[j]==nums[j-1]){
+                        j++;
+                    }
+                    while(k>j && nums[k]==nums[k+1]){
+                        k--;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+};
